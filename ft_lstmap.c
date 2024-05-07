30d27042b1311d9fc7f2e1836c45ba487f1f6474
Yuto <sasayu8190@gmail.com>
2024-05-07 20:17:09 +0900

    fix : lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: yutsasak <yutsasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 18:13:39 by yutsasak          #+#    #+#             */
-/*   Updated: 2024/05/07 20:05:28 by yutsasak         ###   ########.fr       */
+/*   Updated: 2024/05/07 20:16:10 by yutsasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_list;
 	t_list	*new_elem;
 	void	*new_content;
+	void	*new_content;
 
 	new_list = NULL;
 	while (lst)
 	{
 		new_content = f(lst->content);
-		if (!(new_elem = ft_lstnew(new_content)))
+		new_elem = ft_lstnew(new_content);
+		if (!new_elem)
 		{
 			del(new_content);
 			ft_lstclear(&new_list, del);
